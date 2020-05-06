878c1015c9065da028db47f528e81c1d91edec0e
Emma Dalgaard <emla19ac@student.cbs.com>
2020-05-06 11:53:02 +0200

    opdateret database, så den ikke kun kører lokalt

--- backend/src/db.js
+++ backend/src/db.js
@@ -6,7 +6,7 @@ module.exports = {
     //exporter, så andre filer kan tilgå den
     getConnection: async () => {
         if (!connection) {
-            connection = await mongoose.connect("mongodb://localhost/CphEventGear", {
+            connection = await mongoose.connect("mongodb+srv://cpheventgear:cpheventgear123@cluster0-no0xc.mongodb.net/cpheventgear", {
                 useNewUrlParser: true,
                 useCreateIndex: true,
             });

9edd3b65776f245eb9ffb2f2e60edac84b5bda4c
phil2200 <phjo19ab@student.cbs.dk>
2020-05-06 14:11:10 +0200

    implementeret PUT, så man kan ændre pris på evnetpackage

--- frontend/javascript/changeEventPackage.js
+++ frontend/javascript/changeEventPackage.js
@@ -1,9 +1,11 @@
 window.onload = changeEventPackage();
 
+let eventPackageArray = null
+
 async function changeEventPackage() {
     // JSON.parse(localStorage.getItem("customer")
     //window.location = "EditCustomerProfile.html";
-    const eventPackageArray = await (
+    eventPackageArray = await (
         await fetch("http://localhost:3000/eventpackage")
     ).json();
 
@@ -28,6 +30,8 @@ async function changeEventPackage() {
     })
 
 
+
+
 /*
     eventPackageArray.forEach((package) => {
         // En if statement bruges til at tjekke, at det er en current user ved at sammenligne databasens phone med local storage, fordi phone er unikt for ver bruger.
@@ -46,4 +50,70 @@ async function changeEventPackage() {
     })
 */
 
+}
+
+async function showEventPrice() {
+    eventPackageArray = await (
+        await fetch("http://localhost:3000/eventpackage")
+    ).json();
+    var e = document.getElementById("selectNumber");
+    var value = e.options[e.selectedIndex].value;
+   // var text = e.options[e.selectedIndex].text;
+    eventPackageArray.forEach((package) => {
+        if  (package.name == value) {
+            document.getElementById("eventPrice").value = package.price
+        }
+    })
+}
+
+async function updatePrice() {
+    eventPackageArray = await (
+        await fetch("http://localhost:3000/eventpackage")
+    ).json();
+   let newPrice = document.getElementById("eventPrice").value
+   let currentEventpackage_ID = "";
+   let currentName = "";
+   let currentPackageType = "";
+   var e = document.getElementById("selectNumber");
+   var value = e.options[e.selectedIndex].value;
+   let currentPrice = 0;
+    eventPackageArray.forEach((package) => {
+        if  (package.name == value) {
+            currentPrice = package.price;
+            currentEventpackage_ID = package._id;
+            currentName = package.name;
+            currentPackageType = package.packageType;
+        }
+    })
+
+
+    if (isNaN(newPrice))
+    {
+        alert("Du skal indtaste et positivt tal ")
+    }
+    else if (currentName == ""){
+        alert("vælg venligt den pakke du ønsker at opdatere prisen på")
+    }
+    else if (parseInt(newPrice) > 0) {
+        if(newPrice == currentPrice ){
+         alert("Prisen er lig den allerede gemte")
+        }
+        else{
+                let c = new Eventpackage(currentName, newPrice, currentPackageType);
+                await fetch("http://localhost:3000/eventpackage/update/" + currentEventpackage_ID, {
+                    method: 'PUT',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(c)
+                });
+               // localStorage.setItem("customer", JSON.stringify(c));
+                alert("Din brugeroplysninger er nu opdateret");
+               // window.location = "profile.html";
+            }
+        }
+    else {
+        alert ("Cawe")
+    }
 }

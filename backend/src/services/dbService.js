1ddfd4c2ed84be57c8467a2231e71f7ee2abf1e5
phil2200 <phjo19ab@student.cbs.dk>
2020-05-09 09:34:07 +0200

    h

--- backend/src/services/dbService.js
+++ backend/src/services/dbService.js
@@ -2,16 +2,17 @@
 async function find(
     model, // model er en mongoose.model jævnfør de filer som hører ind under models
     query, // gør at vi kan filtrerer på data, fx kan man få alle med: "name": "Emma" ud 
-    projection = { __v: 0 },
-    sort = { _id: 1 },
-    options = { lean: true }
+    projection = { __v: 0 }, //exclude __v from the query
+    sort = { _id: 1 }, //sort using _id in normal order
+    options = { lean: true } //Enabling the lean option tells Mongoose to skip instantiating a full Mongoose document and just give you the POJO(plain own JavaScript objects).
 ) {
-    return model
+    return model // find ud af, hvad denne return egentlig gør
         .find(query, projection, options)
-        .sort(sort)
-        .select({ __v: 0 })
-        .exec();
+      //  .sort(sort)
+      //  .select({ __v: 0 })
+       // .exec();
 }
+
 async function findById(model, id) {
     return model.findById(id);
 }
